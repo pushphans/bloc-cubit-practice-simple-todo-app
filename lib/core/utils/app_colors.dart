b0fe7aba74cfc514d2ea840620dac885a49fca6e
pushp-hans <pushp@zonixsoft.com>
2026-02-03 14:56:03 +0530

    added home screen skeleton

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color grey = Colors.grey;
+  static const Color white = Colors.white;
+}
--- lib/features/home/presentation/screens/home_page.dart
+++ lib/features/home/presentation/screens/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_cubit_practice_simple_todo_app/core/utils/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,7 +13,54 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home"), centerTitle: true),
-      body: SizedBox(child: Center(child: Text("data"))),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            SizedBox(height: 10),
+
+            Card(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 5,
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 11,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.transparent),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    Expanded(flex: 1, child: Icon(Icons.search, size: 30)),
+                  ],
+                ),
+              ),
+            ),
+
+            Expanded(
+              child: ListView.builder(
+                itemCount: 20,
+                itemBuilder: (context, index) {
+                  return ListTile(title: Text("Hi"));
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
